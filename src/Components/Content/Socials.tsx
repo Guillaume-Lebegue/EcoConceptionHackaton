8c1467740f284d0146b37d79bc092038f43af689
Guillaume Lebegue <guillaume.lebegue@epitech.eu>
2023-03-31 16:30:04 +0200

    [UP] Missing alt

--- src/Components/Content/Socials.tsx
+++ src/Components/Content/Socials.tsx
@@ -8,8 +8,8 @@ export default function Socials() {
       <h2>Retrouvez nous</h2>
       <div className='socContainer'>
         <a href="https://www.facebook.com/"> <img srcSet={process.env.PUBLIC_URL+'/vectoriel/facebook.svg'} alt="Facebook redirect"></img></a>
-        <a href="https://www.instagram.com/"> <img srcSet={process.env.PUBLIC_URL+'/vectoriel/instagram.svg'}></img></a>
-        <a href="https://www.linkedin.com/">  <img srcSet={process.env.PUBLIC_URL+'/vectoriel/linkedin.svg'}></img></a>
+        <a href="https://www.instagram.com/"> <img srcSet={process.env.PUBLIC_URL+'/vectoriel/instagram.svg'} alt="Instagram redirect"></img></a>
+        <a href="https://www.linkedin.com/">  <img srcSet={process.env.PUBLIC_URL+'/vectoriel/linkedin.svg'} alt="Linkedin redirect"></img></a>
       </div>
     </div>
   )
