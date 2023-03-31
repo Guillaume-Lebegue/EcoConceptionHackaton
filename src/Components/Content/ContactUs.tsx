e2699e2026703d5b78df0490b1532bb3848bf338
ValentinGandon <valentin.gandon@epitech.eu>
2023-03-31 15:07:33 +0200

    mail
    mail

--- src/Components/Content/ContactUs.tsx
+++ src/Components/Content/ContactUs.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import './Contact.css'; 
 
 export default function ContactUs() {
   return (
-    <div>ContactUs</div>
+    <div className='contactUs'>
+      <h3>Nous contacter</h3>
+      <div className='boxContact'>
+        <div className='form'>
+          <p>
+            Pour toute réservation nous vous invitons à contacter directement le restaurant pas téléphone.
+            <br/><br/>
+            <big>Par mail.</big>
+        </p>
+        <div className='buttonMail'>
+          <a href={`mailto:adresse@exemple.com?subject=Demande d'information&cc=contact@exemple.com&body=${encodeURIComponent("Bonjour je vous contacte pour vous poser une question vis à vis de votre boulangerie")}`}>Envoyer un e-mail</a>
+        </div>
+      </div>
+      <div className='coord'>
+        <h4>Nos coordonnées</h4>
+        <big>Siège Lugnac</big>
+        <p>4 rue Censier, Paris 7<br/><br/>+33 1 42 56 11 26</p>
+      </div>
+      </div>
+    </div>
   )
 }
