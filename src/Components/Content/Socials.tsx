7fe71151906c6ab858f2c869102a95b77b928732
ValentinGandon <valentin.gandon@epitech.eu>
2023-03-31 16:21:31 +0200

    add social

--- src/Components/Content/Socials.tsx
+++ src/Components/Content/Socials.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import './Socials.css';
+
 
 export default function Socials() {
   return (
-    <div>Socials</div>
+    <div className='social'>
+      <h2>Retrouvez nous</h2>
+      <div className='socContainer'>
+        <a href="https://www.facebook.com/"> <img srcSet={process.env.PUBLIC_URL+'/vectoriel/facebook.svg'} alt="Facebook redirect"></img></a>
+        <a href="https://www.instagram.com/"> <img srcSet={process.env.PUBLIC_URL+'/vectoriel/instagram.svg'}></img></a>
+        <a href="https://www.linkedin.com/">  <img srcSet={process.env.PUBLIC_URL+'/vectoriel/linkedin.svg'}></img></a>
+      </div>
+    </div>
   )
 }
